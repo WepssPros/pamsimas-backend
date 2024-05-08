09d28d901a18026b88cf23bab6fdd3197acb5170
WepssPros <reyhandwiambrta@gmail.com>
2024-05-08 13:35:38 +0700

    first

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -8,5 +8,54 @@ use App\Models\Transaction;
 
 class TransactionController extends Controller
 {
-    c
+    public function index(Request $request)
+    {
+
+        $limit = $request->query('limit') !== null ? $request->query('limit') : 10;
+
+        $relations = [
+
+            'paymentMethod:id,name,code,thumbnail',
+            'transactionType:id,name,code,action,thumbnail'
+        ];
+
+        $user = auth()->user();
+
+        $transactions = Transaction::with($relations)
+            ->where('user_id', $user->id)
+            ->where('status', 'success')
+            ->orderBy('id', 'desc')
+            ->paginate($limit);
+
+        $transactions->getCollection()->transform(function ($item) {
+            // Ambil URL thumbnail metode pembayaran atau kosongkan jika tidak tersedia
+            $paymentMethodThumbnail = $item->paymentMethod->thumbnail
+                ? $item->paymentMethod->thumbnail
+                : '';
+
+
+            if ($paymentMethodThumbnail) {
+                $item->paymentMethod->thumbnail = strpos($paymentMethodThumbnail, 'http') === 0
+                    ? $paymentMethodThumbnail
+                    : url('storage/' . $paymentMethodThumbnail);
+            }
+
+
+            $transactionTypeThumbnail = $item->transactionType->thumbnail
+                ? $item->transactionType->thumbnail
+                : '';
+
+
+            if ($transactionTypeThumbnail) {
+                $item->transactionType->thumbnail = strpos($transactionTypeThumbnail, 'http') === 0
+                    ? $transactionTypeThumbnail
+                    : url('storage/' . $transactionTypeThumbnail);
+            }
+
+            return $item;
+        });
+
+
+        return response()->json($transactions);
+    }
 }
